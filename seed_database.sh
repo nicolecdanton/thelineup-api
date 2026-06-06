3d9182485e9c0f75229a6569be7a336a448e910e
Nicole D'Anton <nicoledanton7@gmail.com>
2026-06-06 13:37:12 -0500

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./lineupapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations lineupapi
+python3 manage.py migrate lineupapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
